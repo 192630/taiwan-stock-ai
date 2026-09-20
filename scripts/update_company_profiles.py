f87c1276e42709d74717e36f9e0485a7775d4d26
192630 <eay063025@gmail.com>
2026-09-20 11:49:35 +0800

    將官方產業代碼轉為中文名稱

--- scripts/update_company_profiles.py
+++ scripts/update_company_profiles.py
@@ -10,6 +10,20 @@ SOURCES = (
     ("上市", "https://openapi.twse.com.tw/v1/opendata/t187ap03_L"),
     ("上櫃", "https://www.tpex.org.tw/openapi/v1/mopsfin_t187ap03_O"),
 )
+INDUSTRIES = {
+    "01": "水泥工業", "02": "食品工業", "03": "塑膠工業",
+    "04": "紡織纖維", "05": "電機機械", "06": "電器電纜",
+    "08": "玻璃陶瓷", "09": "造紙工業", "10": "鋼鐵工業",
+    "11": "橡膠工業", "12": "汽車工業", "14": "建材營造",
+    "15": "航運業", "16": "觀光餐旅", "17": "金融保險",
+    "18": "貿易百貨", "19": "綜合", "20": "其他",
+    "21": "化學工業", "22": "生技醫療", "23": "油電燃氣",
+    "24": "半導體業", "25": "電腦及週邊設備業", "26": "光電業",
+    "27": "通信網路業", "28": "電子零組件業", "29": "電子通路業",
+    "30": "資訊服務業", "31": "其他電子業", "32": "文化創意業",
+    "33": "農業科技業", "34": "電子商務", "35": "綠能環保",
+    "36": "數位雲端", "37": "運動休閒", "38": "居家生活",
+}
 
 
 def fetch_json(url):
@@ -43,12 +57,14 @@ def normalize(row, market):
     code = first(row, "公司代號", "公司代码", "SecuritiesCompanyCode")
     if not code:
         return None
+    industry_code = first(row, "產業別", "产业别")
     return {
         "code": code,
         "market": market,
         "company_name": first(row, "公司名稱", "公司名称"),
         "short_name": first(row, "公司簡稱", "公司简称"),
-        "industry": first(row, "產業別", "产业别"),
+        "industry_code": industry_code,
+        "industry": INDUSTRIES.get(industry_code, industry_code),
         "founded_date": normalize_date(first(row, "成立日期")),
         "listed_date": normalize_date(first(row, "上市日期", "上櫃日期", "上柜日期")),
         "address": first(row, "住址", "地址"),
